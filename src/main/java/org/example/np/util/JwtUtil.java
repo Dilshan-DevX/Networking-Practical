8224d9629994fa7100b6f7733c737c172dacfe90
User <tharindu2003wxyz@gmail.com>
2026-02-12 02:00:09 +0530

    4nd commit

--- src/main/java/org/example/np/util/JwtUtil.java
+++ src/main/java/org/example/np/util/JwtUtil.java
@@ -1,9 +1,21 @@
 package org.example.np.util;
 
-public class JwtUtil {
-    private static final String SECRET = "DevX@Dilshan";
-    public static String genarateToken(String email) {
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
-        return null;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final String SECRET = "javax.crypto.spec.SecretKeySpec@58824b0";
+
+    public static String genarateToken(String email) {
+       return Jwts.builder()
+                .setSubject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 24 * 60 * 1000)).
+                signWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .compact();
+
     }
 }
